ba58042f1080fdfa40a17d58dbdc19fc3950bd6b
sachsd <sachsd1116@outlook.com>
2020-11-17 10:56:58 -0500

    push

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { NavLink, withRouter } from 'react-router-dom'
 
 const Navbar = (props) => {
     return (
-        <nav className="nav-wrapper red darken-3">
+        <nav className="nav-wrapper darken-3">
             <div className="container">
                 <a href="/" className="brand-logo"><div className="center title">Texas Hold'em Facts</div></a>
                 <ol className="center">
